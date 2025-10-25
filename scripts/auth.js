ab24ede00f5f14c921dcd629a4cc608a603e8976
Katie-N <kaitlyn.nordberg@gmail.com>
2025-10-25 17:37:10 -0500

    added setting to login with redirect on mobile and popup on desktop. Need to test

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,15 +1,22 @@
-import { getAuth, signInWithPopup, onAuthStateChanged, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getAuth, signInWithPopup, signInWithRedirect, getRedirectResult, onAuthStateChanged, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { populateCalendar, unpopulateCalendar } from "./populateCalendar.js";
 
 window.auth = getAuth(window.app);
 
+function isMobileDevice() {
+  return /Mobi|Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent) || (window.innerWidth <= 768 && window.innerHeight <= 1024);
+}
+
 document.getElementById("signInButton").addEventListener("click", async () => {
-    // signInWithRedirect doesn't work unless you setup a reverse proxy :(
-    // signInWithRedirect(auth, new GoogleAuthProvider());
-    const userCred = await signInWithPopup(window.auth, new GoogleAuthProvider());
-    window.userId = userCred.user.uid
-    if (document.getElementById("optionsContainer").classList.contains("show")) {
-        toggleShow()
+    if (isMobileDevice()) {
+        // signInWithRedirect doesn't work unless you setup a reverse proxy :(
+        // So I switched from GitHub pages to Netlify and added the _redirects file (plus I changed the auth domain in the firebase config and in the firebase console. And I changed the google cloud OAuth client redirect URIs and authorized JavaScript origins.)
+        signInWithRedirect(auth, new GoogleAuthProvider());
+    } else {
+        await signInWithPopup(window.auth, new GoogleAuthProvider())
+        if (document.getElementById("optionsContainer").classList.contains("show")) {
+            toggleShow()
+        }
     }
 });
 
@@ -19,8 +26,25 @@ document.getElementById("signInButton").addEventListener("click", async () => {
 onAuthStateChanged(auth, (user) => {
     // When the user signs in, the calendar should be repopulated with the new user
     if (user) {
+        console.log("onAuthStateChanged", user)
+        window.userId = user.uid
         populateCalendar()
     } else {
         unpopulateCalendar()
     }
 });
+
+// try {
+//     const result = await getRedirectResult(auth);
+//     if (result) {
+//       // This means a redirect sign-in just completed successfully.
+//       const user = result.user;
+//       console.log("Redirect sign-in successful:", user);
+//       // The onAuthStateChanged listener above will also be triggered with this user.
+//     } else {
+//       // No redirect result found, meaning it's either a fresh page load
+//       // or the user was already signed in (handled by onAuthStateChanged).
+//     }
+// } catch (error) {
+//     console.error("Error getting redirect result:", error);
+// }
